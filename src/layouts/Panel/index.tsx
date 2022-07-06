dbc902fec972f0f3f05093bd0ff7529ab75d8aad
Vadym Iefremov <vadym.iefremov@gmail.com>
2022-07-06 12:32:22 +0300

    Add "go back" button to the panel layout

--- src/layouts/Panel/index.tsx
+++ src/layouts/Panel/index.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './panel.scss';
 
 export interface PanelProps {
@@ -7,6 +8,7 @@ export interface PanelProps {
   readonly extensions?: ReactNode;
   readonly children?: ReactNode;
   readonly collapseExtensions?: boolean;
+  readonly displayBackNavigation?: boolean;
 }
 
 export default function Panel(props: PanelProps) {
@@ -15,16 +17,26 @@ export default function Panel(props: PanelProps) {
     headerTools, 
     children, 
     extensions, 
-    collapseExtensions = false 
+    collapseExtensions = false ,
+    displayBackNavigation = false,
   } = props;
+
+  const navigate = useNavigate();
 
   return (
     <div className='panel'>
       <header className="panel-header">
-        <div className="panel-title-box">
-          <h1 className="panel-title">
-            {title}
-          </h1>
+        <div className='left-hand-box'>
+          {displayBackNavigation &&
+            <button className='previous-page-button' onClick={() => navigate(-1)}>
+              <i className="fa-solid fa-angle-left fa-3x" />
+            </button>
+          }
+          <div className="panel-title-box">
+            <h1 className="panel-title">
+              {title}
+            </h1>
+          </div>
         </div>
         <div className="panel-tools">
           {headerTools}
